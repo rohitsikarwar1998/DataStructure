93443da3262972c81673e6048f385cf279c84a48
ROHIT SIKARWAR <rohitsikarwar1998@gmail.com>
2020-07-21 18:08:05 +0530

    add some examples

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 static vector<int>arr;
+const int PARTITION_LIMIT=10;
+
+static int count_loop=0;
 
 void insertSort(int start,int end){
     for(int i=start+1;i<=end;i++){
@@ -10,6 +13,7 @@ void insertSort(int start,int end){
                 swap(arr[j],arr[j-1]);
             }
             else break;
+            count_loop++;
         }
         // for(auto n:arr) cout<<n<<" ";
         // cout<<endl;
@@ -24,6 +28,7 @@ int partition(int start,int end){
         if(pivotElement>=arr[i]){
             swap(arr[i],arr[storeIndex++]);
         }
+        count_loop++;
     }
     swap(arr[storeIndex],arr[end]);
     return storeIndex;
@@ -32,10 +37,10 @@ int partition(int start,int end){
 
 void quickSort(int start,int end){
     if(start>=end) return ;
-    if(start-end<=10) {
-        insertSort(start,end);
-        return ;
-    }
+    // if(start-end<=PARTITION_LIMIT) {
+    //     insertSort(start,end);
+    //     return ;
+    // }
     int pivot=partition(start,end);
     quickSort(start,pivot-1);
     quickSort(pivot+1,end);
@@ -43,11 +48,12 @@ void quickSort(int start,int end){
 
 int main(){
 
-      arr={5,2,7,3,4,9,3};
+      arr={52, 37, 63, 14, 17, 8, 6, 25,27,3,6,29,67,30,20,40,100,35};
       quickSort(0,arr.size()-1);
+
+      cout<<count_loop<<endl;
 
       for(auto n:arr){
           cout<<n<<" ";
       }
-
 }
